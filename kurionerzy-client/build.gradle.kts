e3f823dc06089fce21a5b1f8eef26f9e0b123aba
Rafal Jankowski <rafal.jankowski@allegro.pl>
2023-02-03 11:25:44 +0100

    front

--- kurionerzy-client/build.gradle.kts
+++ kurionerzy-client/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.springframework:spring-web:6.0.4")
     implementation("org.springframework.boot:spring-boot-starter-web:3.0.2")
     implementation("org.apache.kafka:kafka-clients:3.3.1")
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
