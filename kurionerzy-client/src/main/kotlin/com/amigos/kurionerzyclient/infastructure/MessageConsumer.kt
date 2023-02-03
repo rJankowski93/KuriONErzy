3e56eccbf677fc9bc2e7f8549c0c02b44cc1f5e4
karol.klimonczyk <karol.klimonczyk@allegro.pl>
2023-02-03 12:28:48 +0100

    questions consumer

--- kurionerzy-client/src/main/kotlin/com/amigos/kurionerzyclient/infastructure/MessageConsumer.kt
+++ kurionerzy-client/src/main/kotlin/com/amigos/kurionerzyclient/infastructure/MessageConsumer.kt
@@ -2,6 +2,8 @@ package com.amigos.kurionerzyclient.infastructure
 
 import com.amigos.kurionerzyclient.domain.Consumer
 import com.amigos.kurionerzyclient.logger
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 import java.util.concurrent.ConcurrentHashMap
@@ -14,7 +16,7 @@ class MessageConsumer : Consumer {
         val questions = ConcurrentHashMap<String, Question>()
     }
 
-    @KafkaListener(topics = ["questions"], groupId = "kurionerzy") // todo change groupId
+    @KafkaListener(topics = ["questions"], groupId = "kurionerzy", containerFactory = "questionContainerFactory") // todo change groupId
     override fun subscribeQuestions(message: Question) {
         questions[message.id] = message
         if (questions.size == 1) {
@@ -29,22 +31,31 @@ class MessageConsumer : Consumer {
                 a) ${message.possibleAnswers.A},
                 b) ${message.possibleAnswers.B},
                 c) ${message.possibleAnswers.C},
-                d) ${message.possibleAnswers.D},
+                d) ${message.possibleAnswers.D}
             """.trimIndent()
         )
     }
 }
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Question(
+    @JsonProperty("id")
     val id: String,
+    @JsonProperty("text")
     val text: String,
+    @JsonProperty("possibleAnswers")
     val possibleAnswers: PossibleAnswers,
+    @JsonProperty("correctAnswer")
     val correctAnswer: String
 )
 
 data class PossibleAnswers(
+    @JsonProperty("A")
     val A: String,
+    @JsonProperty("B")
     val B: String,
+    @JsonProperty("C")
     val C: String,
+    @JsonProperty("D")
     val D: String
 )
