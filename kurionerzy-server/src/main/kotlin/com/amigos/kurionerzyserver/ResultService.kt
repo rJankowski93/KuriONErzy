6dc4396b2f22a2ba59953f4855ae00effd4259a2
BartekG <bartlomiej.golabek@allegro.pl>
2023-02-03 13:06:14 +0100

    GLMP commit main

--- kurionerzy-server/src/main/kotlin/com/amigos/kurionerzyserver/ResultService.kt
+++ kurionerzy-server/src/main/kotlin/com/amigos/kurionerzyserver/ResultService.kt
@@ -1,10 +1,13 @@
 package com.amigos.kurionerzyserver
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
 @Component
-class ResultService(val kafkaTemplate: KafkaTemplate<String, ResultsGame>) {
+class ResultService(
+    @Qualifier("kafkaTemplate")
+    val kafkaTemplate: KafkaTemplate<String, ResultsGame>) {
 
     fun sendResult(resultsGames: ResultsGame) {
         kafkaTemplate.send("results", resultsGames)
